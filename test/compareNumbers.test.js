6a07a8e7aed065cf55f482a93e09b54bd5826f8e
Ian Andrewson <ianandrewson@gmail.com>
2019-10-01 16:47:52 -0700

    implemented unit tests, all tests pass

---
+++ test/compareNumbers.test.js
@@ -0,0 +1,45 @@
+// IMPORT MODULES under test here:
+// import example from '../src/example.js';
+import { compareNumbers } from '../compare-numbers.js';
+
+const test = QUnit.test;
+
+test('should return 0 if numbers are identical', function(assert) {
+    //Arrange
+    // Set up your parameters and expectations
+    const guess = 10;
+    const correctNumber = 10; 
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const result = compareNumbers(guess, correctNumber);
+    //Assert
+    // Make assertions about what is expected valid result
+    assert.equal(result, 0);
+});
+
+test('should return -1 if number guessed number is too low', function(assert) {
+    //Arrange
+    // Set up your parameters and expectations
+    const guess = 8;
+    const correctNumber = 10 ;
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const result = compareNumbers(guess, correctNumber);
+    //Assert
+    // Make assertions about what is expected valid result
+    assert.equal(result, -1);
+});
+
+test('should return 0 if numbers are identical', function(assert) {
+    //Arrange
+    // Set up your parameters and expectations
+    const guess = 12;
+    const correctNumber = 3;
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const result = compareNumbers(guess, correctNumber);
+    //Assert
+    // Make assertions about what is expected valid result
+    assert.equal(result, 1);
+});
+
